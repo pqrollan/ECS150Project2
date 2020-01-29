1f8a894326e2bb6ec1ea9e5d4f67c03aee7fc28e
Ashwin Muralidharan <ashwin.m.dharan@gmail.com>
2020-01-28 22:21:15 -0800

    Added rough draft of queue data structure

--- p2/libuthread/queue.c
+++ p2/libuthread/queue.c
@@ -6,31 +6,65 @@
 
 struct queue {
 	/* TODO Phase 1 */
+	queue_t next;
+	int height;
+	void* data;
+	queue_t first;
 };
 
+//Does the result of queue_create count as an empty queue?
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+	queue_t q =  (queue_t)malloc(sizeof(queue));
+	q->height = 0;
+	q->data = NULL;
+	q->first = NULL;
+	q->next = NULL;
+	return q;
 }
 
 int queue_destroy(queue_t queue)
 {
-	/* TODO Phase 1 */
+	queue = NULL;
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
+	queue_t temp = queue_create();
+	temp->data = data
+	queue->next = temp
+	temp->height= queue->height++;
+	temp->first = queue->first;
+	temp->next = NULL;
+	queue = temp;
+
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
 	/* TODO Phase 1 */
+	if queue->first != NULL{
+		temp = queue->first;
+		queue->first = temp->next;
+		*data = temp->data
+		queue->height--;
+		free(temp);
+		temp = NULL;
+	}
+
+	
 }
 
 int queue_delete(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+	queue_t temp= queue->first;
+	for (int i = 0; i< queue.length(); i++){ 
+		
+		i--;
+		
+	}
+	
 }
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
@@ -41,5 +75,6 @@ int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
 int queue_length(queue_t queue)
 {
 	/* TODO Phase 1 */
+	return queue->length;
 }
 
