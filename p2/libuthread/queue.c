9f651c5e5f364390a9019d283e41120104b3f31c
Ashwin Muralidharan <ashwin.m.dharan@gmail.com>
2020-02-04 15:48:52 -0800

    Changed queue implementation into doubly linked list. Implemented basic test cases.

--- p2/libuthread/queue.c
+++ p2/libuthread/queue.c
@@ -5,24 +5,30 @@
 
 #include "queue.h"
 
+struct node {
+	void* data;
+	struct node* prev;
+	struct node* next;
+};
+
 struct queue {
 	/* TODO Phase 1 */
-	queue_t next;
+	struct node* first;
+	struct node* last;
 	int height;
-	void* data;
-	queue_t first;
 };
+
+
 
 //Does the result of queue_create count as an empty queue?
 queue_t queue_create(void)
 {
-	printf("making space\n");
+	//printf("making space\n");
 	queue_t q =  (queue_t)malloc(sizeof(struct queue));
 	q->height = 0;
-	q->data = NULL;
 	q->first = NULL;
-	q->next = NULL;
-	printf("new queue created\n");
+	q->last = NULL;
+	//printf("new queue created\n");
 	return q;
 }
 
@@ -33,48 +39,30 @@ int queue_destroy(queue_t queue){
 	free(queue);
 	queue = NULL;
 	if (queue== NULL){
-	// 	printf("working queue\n");
 		printf("queue destroyed\n");
-
 	}
 	return 0;
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
-	printf("enqueue before test\n");
+	
 	if (queue == NULL || data == NULL){
 		return -1;
 	}
-	printf("enqueue start \n");
 
-	queue_t temp = queue_create();
-	if (queue->height==0){
-		printf("Queue's height is 0\n");
-		queue->data = data;
-		queue->height = 1;
-		queue->first = queue;
-		printf("first queue item assigned\n");
+	struct node* temp = (struct node*)malloc(sizeof(struct node));
+
+	temp->data = data;
+	temp->prev = queue->last;
+	temp->next = NULL;
+	queue->last = temp;
+	queue->height = queue->height+ 1;
+	if (queue->height == 1){
+		queue->first = temp;
 	}
 	else{
-		printf("Queue's height is %d\n", queue->height);
-		printf("new queue height SHOULD BE %d\n", queue->height + 1);
-		// temp = queue_create();
-		if (temp == NULL){
-			return -1;
-		}
-		printf("after temp made %d\n", queue->height + 1);
-		temp->data = data;
-		temp->height= queue->height;
-		temp->first = queue->first;
-		temp->next = NULL;
-		queue->next = temp;
-		queue = temp;
-		//(*queue) =(*temp);
-		queue->height = queue->height+ 1;
-		printf("the NEW Queue's height is %d\n", queue->height);
-		
-		printf("queue item added\n");
+		temp->prev->next = temp;
 	}
 	
 	return 0;
@@ -86,56 +74,46 @@ int queue_dequeue(queue_t queue, void **data)
 	if (queue == NULL || data == NULL || queue->height == 0){
 		return -1;
 	}
-	if (queue->height == 1){
-		*data = queue->data;
-		queue->height = 0;
-		queue->data = NULL;
-		queue->first = NULL;
-		queue->next = NULL;
+	
+	struct node* temp = queue->first;
+	*data = temp->data;
+	queue->first = temp->next;
+	queue->height = queue->height -1;
+
+	if (queue->height > 0){
+		queue->first->prev = NULL;
 	}
 	else{
-		queue_t temp = queue->first;
-		printf("temp is assigned\n");
-
-
-		/*
-
-			this is where things would seg fault
-
-		*/
-		
-		queue->first = queue->first->next;
-		printf("queue first is assigned\n");
-		*data = temp->data;
-		printf("data is assigned\n");
-		queue->height = queue->height - 1;
-		printf("height is reassigned\n");
-		free(temp);
-		temp = NULL;
+		queue->last= NULL;
 	}
-	printf("item dequeued\n");
+
 	return 0;
 	
 }
 
 int queue_delete(queue_t queue, void *data)
 {
-	if (queue == NULL || data == NULL){
+	if (queue == NULL || data == NULL || queue_length(queue) == 0){
 		return -1;
 	}
-	queue_t temp= queue->first;
-	if(temp->data == data){
-		return queue_dequeue(queue, NULL);
-	}
-	for (int i = 0; i < queue_length(queue)-1; i++){ 
-		if (temp->next->data == data){
-			queue_t toDelete = temp->next;
-			temp->next = temp->next->next;
+	struct node* temp= queue->first;
+
+	for (int i = 0; i < queue_length(queue); i++){ 
+		if (temp->data == data){
+			if(i!=0){
+				temp->prev->next = temp->next;
+			}
+			else{
+				queue->first= temp->next;
+			}
+			if ((i+1) != queue_length(queue)){
+				temp->next->prev = temp->prev;
+			}
+			else{
+				queue->last= temp->prev;
+			}
 			queue->height--;
-			temp->height = queue->height;
-			temp->first = queue->first;
-			free(toDelete);
-			printf("item deleted\n");
+			free (temp);
 			return 0;
 		}
 		temp=temp->next;
@@ -146,15 +124,15 @@ int queue_delete(queue_t queue, void *data)
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
 {
-	if (queue == NULL || data == NULL){
+	if (queue == NULL || func == NULL){
 		return -1;
 	}
 
-	queue_t temp= queue->first;
+	struct node* temp = queue->first;
 	for (int i = 0; i < queue_length(queue); i++){ 
 		int returned = func(temp->data, arg);
 		if (returned == 1){
-			if (*data != NULL){
+			if (data != NULL){
 				*data = temp->data;
 			}
 			printf("item found, iteration stops\n");
@@ -172,7 +150,7 @@ int queue_length(queue_t queue)
 		return -1;
 	}
 	printf("height = %d\n", queue->height);
-	printf("about to return height:\n");
+	//printf("about to return height:\n");
 	return (queue->height);
 }
 
--- p2/libuthread/test_queue.c
+++ p2/libuthread/test_queue.c
@@ -25,6 +25,7 @@ void test_queue_simple(void)
     queue_length(q);
     queue_dequeue(q, (void**)&ptr);
     queue_length(q);
+    assert(queue_length(q) == 0);
     assert(ptr == &data);
 }
 
@@ -37,42 +38,181 @@ void test_queue_destroy(){
 	assert(i == 0);
 }
 
-void test_enqueue_dequeue(void){
-	printf(" \n \n \n Test enqueue dequeue \n \n \n");
+void test_height_dequeue_test(void){
+	printf(" \n \n \n Test dequeue height\n \n \n");
 	queue_t q;
     int data = 3, *ptr;
 
     q = queue_create();
     queue_enqueue(q, &data);
-    queue_length(q);
     queue_enqueue(q, &data);
-    queue_length(q);
     queue_dequeue(q, (void**)&ptr);
-    queue_length(q);
     queue_dequeue(q, (void**)&ptr);
-    queue_length(q);
     queue_enqueue(q, &data);
-    queue_length(q);
     queue_enqueue(q, &data);
-    queue_length(q);
 	queue_enqueue(q, &data);
-	queue_length(q);
     queue_enqueue(q, &data);
-    queue_length(q);
     queue_dequeue(q, (void**)&ptr);
-    queue_length(q);
     queue_dequeue(q, (void**)&ptr);
-    queue_length(q);
 
+    assert(queue_length(q) == 2);
+}
+
+void test_height_delete_test(void){
+    printf(" \n \n \n Test delete height\n");
+    queue_t q;
+    int data = 3;
+
+    q = queue_create();
+    assert(queue_delete(q, &data)==-1);
+    queue_enqueue(q, &data);
+    queue_enqueue(q, &data);
+    queue_delete(q, &data);
+    queue_delete(q, &data);
+    queue_enqueue(q, &data);
+    queue_enqueue(q, &data);
+    queue_enqueue(q, &data);
+    queue_enqueue(q, &data);
+    queue_delete(q, &data);
+    queue_delete(q, &data);
+
+    assert(queue_length(q) == 2);
+}
+
+
+void test_simple_delete(void)
+{
+    printf("Test delete simple\n");
+    queue_t q;
+    int data = 3;
+    int dataValid = 2;
+
+
+    q = queue_create();
+    queue_enqueue(q, &data);
+    queue_enqueue(q, &dataValid);
+    queue_enqueue(q, &data);
+    queue_delete(q,&dataValid);
+    /*
+        testing that 1 was deleted and that there exists no other datavalid
+    */
+    assert(queue_length(q) == 2);
+    assert(queue_delete(q,&dataValid)==-1);
+
+    queue_delete(q,&data);
+    queue_delete(q,&data);
+    /*
+        testing that we can successfully delete the last 2 items
+    */
+    assert(queue_length(q) == 0);
 
 }
+
+void test_delete_first_element(void){
+    printf("Test delete first element\n");
+    queue_t q;
+    int dataValid = 3;
+    int data = 2, *ptr;
+
+
+    q = queue_create();
+    queue_enqueue(q, &dataValid);
+    queue_enqueue(q, &data);
+    queue_delete(q,&dataValid);
+    
+    //Test height of queue after delete.
+    assert(queue_length(q) == 1);
+    assert(queue_delete(q,&dataValid)==-1); //Test whether dataValid still exists in the queue
+    
+    queue_dequeue(q, (void**)&ptr);
+    assert(ptr == &data); //Test whether the remaining element is the correct one.
+
+    assert(queue_delete(q, &data) == -1); //Test whether data is still in the queue.
+}
+
+
+void test_delete_last_element(void){
+    printf("Test delete last element\n");
+    queue_t q;
+    int dataValid = 3;
+    int data = 2, *ptr;
+
+
+    q = queue_create();
+    queue_enqueue(q, &data);
+    queue_enqueue(q, &dataValid);
+    queue_delete(q,&dataValid);
+    
+    //Test height of queue after delete.
+    assert(queue_length(q) == 1);
+    assert(queue_delete(q,&dataValid)==-1); //Test whether dataValid still exists in the queue
+    
+    queue_dequeue(q, (void**)&ptr);
+    assert(ptr == &data); //Test whether the remaining element is the correct one.
+
+    assert(queue_delete(q, &data) == -1); //Test whether data is still in the queue.
+}
+
+/* Callback function that increments items by a certain value */
+static int inc_item(void *data, void *arg)
+{
+    int *a = (int*)data;
+    int inc = (int)(long)arg;
+
+    *a += inc;
+
+    return 0;
+}
+
+/* Callback function that finds a certain item according to its value */
+static int find_item(void *data, void *arg)
+{
+    int *a = (int*)data;
+    int match = (int)(long)arg;
+
+    if (*a == match)
+        return 1;
+
+    return 0;
+}
+
+void test_iterator(void)
+{
+    queue_t q;
+    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int i;
+    int *ptr;
+
+    /* Initialize the queue and enqueue items */
+    q = queue_create();
+    for (i = 0; i < (int)(sizeof(data) / sizeof(data[0])); i++)
+        queue_enqueue(q, &data[i]);
+
+    /* Add value '1' to every item of the queue */
+    queue_iterate(q, inc_item, (void*)1, NULL);
+    assert(data[0] == 2);
+
+    /* Find and get the item which is equal to value '5' */
+    ptr = NULL;
+    queue_iterate(q, find_item, (void*)5, (void**)&ptr);
+    assert(ptr != NULL);
+    assert(*ptr == 5);
+    assert(ptr == &data[3]);
+}
+
 
 
 int main(){
 	//test_create();
 	test_queue_simple();
 	//test_queue_destroy();
-	test_enqueue_dequeue();
+	test_height_dequeue_test();
+    test_height_delete_test();
+
+    test_simple_delete();
+    test_delete_first_element();
+    test_delete_last_element();
+    test_iterator();
 
 }
 
