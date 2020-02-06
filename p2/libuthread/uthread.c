3dae9c098a3984ddffc5fc9babda77bac80a933e
Ashwin Muralidharan <ashwin.m.dharan@gmail.com>
2020-02-05 17:46:26 -0800

    Receiving segfault when calling swapcontext. Don't know why it's happening.

--- p2/libuthread/uthread.c
+++ p2/libuthread/uthread.c
@@ -27,7 +27,7 @@ queue_t blockedQueue;
 struct TCB {
 	uthread_t tid;
 	void* stack;
-	uthread_ctx_t context;
+	uthread_ctx_t* context;
 	enum uthread_State state;
 	int retval;
 
@@ -35,27 +35,31 @@ struct TCB {
 
 struct TCB* runningThread;
 
-struct TCB* tbcArray[USHRT_MAX]= {NULL};
+struct TCB* tcbArray[USHRT_MAX]= {NULL};
 
 /* TODO Phase 2 */
 
 
 void uthread_yield(void)
 {
-	struct TCB* temp = NULL;
-	struct TCB* temp_context = NULL;
+	struct TCB* next_thread = (struct TCB*) malloc (sizeof(struct TCB));
+	//struct TCB* curr_context = (struct TCB*) malloc (sizeof(struct TCB));
+	uthread_t runningThreadTid = runningThread->tid;
 
 	if (runningThread){
 		printf("running thread is not null\n");
 	}
-	queue_enqueue(readyQueue, (void *)runningThread); //Enqueue the running thread to the ready queue
-	queue_dequeue(readyQueue, (void**) &temp); //Get the next thread ready to run
+	queue_enqueue(blockedQueue, (void *)tcbArray[runningThreadTid]); //Enqueue the running thread to the ready queue
+	queue_dequeue(readyQueue, (void**) &next_thread); //Get the next thread ready to run
 	printf("enqueue dequeue success\n");
-	temp_context = runningThread;
-	runningThread = temp;
+	runningThread = next_thread;
 	printf("entering context switch\n");
+
+	if(next_thread){
+		printf("temp exists\n");
+	}
 	
-	uthread_ctx_switch(&(temp_context->context), &(temp->context)); //Switch the contexts
+	uthread_ctx_switch((tcbArray[runningThreadTid]->context), (runningThread->context)); //Switch the contexts
 	 //Switch the running thread
 }
 
@@ -69,33 +73,33 @@ int uthread_create(uthread_func_t func, void *arg)
 	if(tid_count < USHRT_MAX){
 		//If main:
 		printf("Create_thread start\n");
-		uthread_ctx_t c;
+		uthread_ctx_t* c = (uthread_ctx_t*)malloc(sizeof(uthread_ctx_t));
 		void* s = uthread_ctx_alloc_stack();
 		struct TCB* t = (struct TCB*) malloc (sizeof(struct TCB));
 
 		
 		if(tid_count == 0){
 			printf("Creating main thread\n");
-			uthread_ctx_t c_main;
+			uthread_ctx_t* c_main = (uthread_ctx_t*)malloc(sizeof(uthread_ctx_t));
 			void* s_main = uthread_ctx_alloc_stack();
 			runningThread = (struct TCB*) malloc (sizeof(struct TCB));
 			printf("space allocated in main\n");
-			uthread_ctx_switch(&c_main, &c_main);
-			struct TCB temp= {0, s_main, c_main, RUNNING, -1};
-			(*runningThread) = temp;
+			uthread_ctx_switch(c_main, c_main);
+			*runningThread = (struct TCB){0, s_main, c_main, RUNNING, -1};
+			
 			printf("running thread assigned\n");
 			readyQueue = queue_create();
 			blockedQueue = queue_create();
-			tbcArray[tid_count]= runningThread;
+			tcbArray[tid_count]= runningThread;
 			tid_count++;
 		}
 
 		printf("Creating a new thread\n");
-		uthread_ctx_init(&c, s, func, arg);
+		uthread_ctx_init(c, s, func, arg);
 		struct TCB temp= {tid_count, s, c, READY, -1};
 		*t = temp;
 		queue_enqueue(readyQueue, &t);
-		tbcArray[tid_count]= t;
+		tcbArray[tid_count]= t;
 		tid_count++;
 		printf("accessing t?\n");
 		return t->tid;
@@ -106,16 +110,26 @@ int uthread_create(uthread_func_t func, void *arg)
 
 void uthread_exit(int retval)
 {
-	struct TCB* temp;
-	//Set the running thread's return value to retval
+	printf("Entered uthread_exit\n");
+	struct TCB* next_thread;
+	//Set the runningg thread's return value to retval
 	runningThread->retval = retval;
 	//Set the status of the thread to terminated
 	runningThread->state = ZOMBIE;
 	//Switch to the next ready thread
-	queue_dequeue(readyQueue, (void**) &temp); //Get the next thread ready to run
-	uthread_ctx_switch(&runningThread->context, &temp->context); //Switch the contexts
-	(*runningThread) = (*temp); //Switch the running thread
+	uthread_t runningThreadTid = runningThread->tid;
 
+	if (queue_length(readyQueue)>0){
+		queue_dequeue(readyQueue, (void**) &next_thread); //Get the next thread ready to run
+		(*runningThread) = (*next_thread); //Switch the running thread
+		uthread_ctx_switch((tcbArray[runningThreadTid]->context), next_thread->context); //Switch the contexts
+		
+	}
+	else{
+		(*runningThread) = *tcbArray[0]; 
+		uthread_ctx_switch((tcbArray[runningThreadTid]->context), (tcbArray[0])->context);
+	}
+	
 }
 
 
@@ -147,6 +161,7 @@ int uthread_join(uthread_t tid, int *retval)
  			*/
  		}
  	}
+ 	printf("attempting to destroy\n");
  	queue_destroy(readyQueue);
 
 
